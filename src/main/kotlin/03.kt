cd15195f3253d0670b83efe27f5744b1734d8eb6
Ármin Scipiades <scipiades.armin@gmail.com>
2023-12-03 21:47:02 +0100

    03 b

--- src/main/kotlin/03.kt
+++ src/main/kotlin/03.kt
@@ -5,48 +5,52 @@ fun main() {
     {}::class.java.getResourceAsStream("03_a")!!.bufferedReader().use { reader ->
 
         val engine = Engine(reader.lines().filter({ !it.isBlank() }).toList())
-        val partNumbers = engine.examine()
-        println(partNumbers.sum())
+        val result = engine.examine()
+        println(result)
 
     }
 }
 
 class Engine(val map: List<String>) {
-    fun examine(): List<Int> {
-        val result = mutableListOf<Int>()
+    val width = map[0].length;
+
+    fun examine(): Int {
         var currentNumber = "";
-        var numberIsObjectLabel = false;
+        var gearsAroundCurrentNumber = mutableListOf<Int>()
+        val numbersByGears = mutableMapOf<Int, MutableSet<Int>>()
 
         for ((y, line) in map.withIndex()) {
             for ((x, char) in line.withIndex()) {
                 if (char.isDigit()) {
-                    currentNumber += char;
-                    if (thereIsSymbolAround(x, y)) {
-                        numberIsObjectLabel = true;
+                    currentNumber += char
+                    val gearAround = hasGearAround(x, y)
+                    if (gearAround >= 0) {
+                        gearsAroundCurrentNumber.add(gearAround)
                     }
                 } else {
-                    if (numberIsObjectLabel) {
-                        result.add(currentNumber.toInt())
+                    val uniqueGears = gearsAroundCurrentNumber.distinct();
+                    uniqueGears.forEach {
+                        numbersByGears.getOrPut(it) { mutableSetOf() }.add(currentNumber.toInt())
                     }
 
+                    gearsAroundCurrentNumber = mutableListOf();
                     currentNumber = "";
-                    numberIsObjectLabel = false;
                 }
             }
         }
 
-        return result
+        return numbersByGears.values.stream().filter { it.size == 2 }.mapToInt { it.stream().reduce { x, y -> x*y}.orElse(0) }.sum()
     }
 
-    private fun thereIsSymbolAround(originX: Int, originY: Int): Boolean {
+    private fun hasGearAround(originX: Int, originY: Int): Int {
         for (y in (max(originY-1, 0)..min(originY+1, map.size-1))) {
             for (x in (max(originX-1, 0)..min(originX+1, map[y].length-1))){
-                if (map[y][x].let { !it.isDigit() && it != '.' }){
-                    return true
+                if (map[y][x] == '*'){
+                    return y * width + x
                 }
             }
         }
 
-        return false;
+        return -1;
     }
 }
